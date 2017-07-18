08b0592fe126ba9810e137e8c239b359c74f068f
sshah98 <shahsuraj261@gmail.com>
2017-07-18 16:59:47 +0530

    start of stock prediction model

--- stock_data.py
+++ stock_data.py
@@ -1,38 +1,30 @@
 import quandl
-import pandas as pd
+import datetime as dt
 import matplotlib.pyplot as plt
 from matplotlib import style
+import pandas as pd
+import pandas_datareader.data as web
+from matplotlib.finance import candlestick_ohlc
+import matplotlib.dates as mdates
 
-style.use('fivethirtyeight')
+style.use('ggplot')
 
-value = open('/home/suraj/Documents/Programming/quandlapikey.txt', 'r').read()
-value = value.rstrip('\n')
+start = dt.datetime(2000,1,1)
+end = dt.datetime.today()
+df = web.DataReader('TSLA', 'google', start, end)
 
-quandl.ApiConfig.api_key = '%s' % value
+df_ohlc = df['Close'].resample('10D').ohlc()
 
-data = quandl.get_table('WIKI/PRICES', ticker='SWI')
+df_volume = df['Volume'].resample('10D').sum()
+# print(df_ohlc.head())
 
-df = pd.DataFrame(data)
+df_ohlc = df_ohlc.reset_index()
+df_ohlc['Date'] = df_ohlc['Date'].map(mdates.date2num)
 
-data = df[['ticker', 'date', 'high', 'low', 'close']].copy()
-
-
-
-# fig = plt.figure()
-# ax1 = plt.subplot2grid((2,1), (0,0))
-# ax2 = plt.subplot2grid((2,1), (1,0), sharex=ax1)
-# data.plot(x='date', y=['high', 'low'], ax=ax1)
-# data.plot(x='date', y='close', ax=ax2)
-# plt.show()
-
-#
-# from zipline.api import order, record, symbol
-#
-#
-# def initialize(context):
-#     pass
-#
-#
-# def handle_data(context, data):
-#     order(symbol('AAPL'), 10)
-#     record(AAPL=data.current(symbol('AAPL'), 'price'))
+fig = plt.figure()
+ax1 = plt.subplot2grid((6,1), (0,0), rowspan=5, colspan=1)
+ax2 = plt.subplot2grid((6,1), (5,0), rowspan=1, colspan=1,sharex=ax1)
+ax1.xaxis_date()
+candlestick_ohlc(ax1, df_ohlc.values, width=2, colorup='g')
+ax2.fill_between(df_volume.index.map(mdates.date2num),df_volume.values,0)
+plt.show()
