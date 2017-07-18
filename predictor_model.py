137f36672bbbf9eee0eac1bf7509cc935b96d2cf
sshah98 <shahsuraj261@gmail.com>
2017-07-18 18:16:54 +0530

    completed slope calculation of regression line

---
+++ predictor_model.py
@@ -0,0 +1,36 @@
+import quandl
+import datetime as dt
+import matplotlib.pyplot as plt
+from matplotlib import style
+import pandas as pd
+import pandas_datareader.data as web
+from matplotlib.finance import candlestick_ohlc
+import matplotlib.dates as mdates
+import numpy as np
+from statistics import mean
+
+
+style.use('ggplot')
+
+start = dt.datetime(2000,1,1)
+end = dt.datetime.today()
+df = web.DataReader('TSLA', 'google', start, end)
+
+df_ohlc = df['Close'].resample('10D').ohlc()
+df_ohlc = df_ohlc.reset_index()
+df_ohlc['Date'] = df_ohlc['Date'].map(mdates.date2num)
+
+x = np.array(df_ohlc['Date'], dtype=np.float64)
+y = np.array(df_ohlc['close'], dtype=np.float64)
+
+def best_fit_slope(x,y):
+    m = (((mean(x) * mean(y)) - mean(x * y)) /
+         ((mean(x) ** 2) - mean(x ** 2)))
+    return m
+
+m = best_fit_slope(x, y)
+print(m)
+
+
+
+# print(df_ohlc.head())
