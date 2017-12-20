3652d69e2f4e5bfa12f8a305d4fe9da4ea7aef74
Suraj <shahsuraj261@gmail.com>
2017-12-20 14:48:04 -0500

    edited files

--- predictor_model.py
+++ predictor_model.py
@@ -23,14 +23,19 @@ df_ohlc['Date'] = df_ohlc['Date'].map(mdates.date2num)
 x = np.array(df_ohlc['Date'], dtype=np.float64)
 y = np.array(df_ohlc['close'], dtype=np.float64)
 
-def best_fit_slope(x,y):
+def best_fit_slope_and_intercept(x,y):
     m = (((mean(x) * mean(y)) - mean(x * y)) /
          ((mean(x) ** 2) - mean(x ** 2)))
-    return m
 
-m = best_fit_slope(x, y)
-print(m)
+    b = mean(y) - m*mean(x)
+
+    return m, b
+
+m,b = best_fit_slope_and_intercept(x, y)
+# print(m,b)
 
 
 
+df_ohlc['close'].plot()
+plt.show()
 # print(df_ohlc.head())
