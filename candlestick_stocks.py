b7496218b4abf0967ac7a4947bcf09db610de3e3
sshah98 <shahsuraj261@gmail.com>
2017-12-28 13:08:21 -0500

    added files for learning

--- candlestick_stocks.py
+++ candlestick_stocks.py
@@ -7,6 +7,7 @@ from matplotlib.finance import candlestick_ohlc
 import matplotlib.dates as mdates
 
 
+
 style.use('ggplot')
 
 start = dt.datetime(2000, 1, 1)
@@ -20,4 +21,14 @@ df = pd.read_csv('TSLA.csv', parse_dates=True, index_col=0)
 df_ohlc = df['Adj Close'].resample('10D').ohlc()
 df_volume = df['Volume'].resample('10D').sum()
 
-print(df_ohlc.head())
+# print(df_ohlc.head())
+df_ohlc = df_ohlc.reset_index()
+df_ohlc['Date'] = df_ohlc['Date'].map(mdates.date2num)
+fig = plt.figure()
+ax1 = plt.subplot2grid((6,1), (0,0), rowspan=5, colspan=1)
+ax2 = plt.subplot2grid((6,1), (5,0), rowspan=1, colspan=1, sharex=ax1)
+ax1.xaxis_date()
+
+candlestick_ohlc(ax1, df_ohlc.values, width=2, colorup='g')
+ax2.fill_between(df_volume.index.map(mdates.date2num), df_volume.values, 0)
+plt.show()
